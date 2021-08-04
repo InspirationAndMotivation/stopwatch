28e53c9911bd76c2f7407238a0d9036aa534396f
Anastasiia Pranovych <pranovych.anastasiia@gmail.com>
2021-08-04 12:44:27 +0300

    Fixed time formatting problems

--- src/utils/timeFormattingUtil.js
+++ src/utils/timeFormattingUtil.js
@@ -5,11 +5,14 @@ const setTimeFormat = (totalTime) => {
     const hours = Math.floor(totalTime / 3600);
       
     const formatedSeconds = (sec < 10) ? `0${sec}` : `${sec}`;
-    const formatedMinutes = (min < 60) ? ((min === 0) ? '00' : `0${min}`) : `${min}`;  
-    const formatedHours = ( hours > 23|| hours < 1) ? '00' : (hours > 0 && hours < 10) ? `0${hours}` : `${hours}`;
+    const formatedMinutes = (min > 60) ? 
+                            ((min-60*hours < 10) ? `0${min-60*hours}` : `${min-60*hours}`) : 
+                            ((min < 10) ? ((min === 0) ? `00` : `0${min}`) : `${min}`) ;  
+    const formatedHours = (hours < 1) ? '00' : (hours > 0 && hours < 10) ? `0${hours}` : `${hours}`;
 
     return `${formatedHours}:${formatedMinutes}:${formatedSeconds}`;
   };
   
   export default setTimeFormat;
+  
   
